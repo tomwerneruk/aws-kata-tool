2d56338ea58da05069ed55fd223b8a415519722d
Tom Werner <tom.werner@version1.com>
2023-09-06 13:03:40 +0100

    Update CLI Root help string

--- cmd/root.go
+++ cmd/root.go
@@ -30,16 +30,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "aws-kata-tool",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "aws-kata-tool is a 'stub' that can be used as a placeholder service for demos or gamedays, or as a test harness at different points of your project.",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
